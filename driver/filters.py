14d5a9e0446cb4a87ab0ea7da8ecba1e42bab4f9
Unknown <98932323+P9P9@users.noreply.github.com>
2022-05-15 16:02:53 +0300

    Update filters.py

--- driver/filters.py
+++ driver/filters.py
@@ -5,7 +5,7 @@ from config import COMMAND_PREFIXES
 
 other_filters = filters.group & ~filters.via_bot & ~filters.forwarded
 other_filters2 = (
-    filters.private & ~filters.edited & ~filters.via_bot & ~filters.forwarded
+    filters.private & ~filters.via_bot & ~filters.forwarded
 )
 
 
