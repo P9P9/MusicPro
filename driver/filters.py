062f1492d527ac0d953e0a782061bc0689f49947
Unknown <98932323+P9P9@users.noreply.github.com>
2022-05-15 14:59:31 +0300

    Update filters.py

--- driver/filters.py
+++ driver/filters.py
@@ -3,7 +3,7 @@ from typing import List, Union
 from config import COMMAND_PREFIXES
 
 
-other_filters = filters.group & ~filters.edited & ~filters.via_bot & ~filters.forwarded
+other_filters = filters.group & ~filters.via_bot & ~filters.forwarded
 other_filters2 = (
     filters.private & ~filters.edited & ~filters.via_bot & ~filters.forwarded
 )
